971281e0ed88090827091f24ff7a2973ade0b08e
Hooo1941 <jiahao1941@outlook.com>
2022-09-16 22:12:07 +0800

    bugfix: router cannot reload in production mode use react-router-dom v6 and @loadable/component

--- src/App.tsx
+++ src/App.tsx
@@ -4,57 +4,61 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import CssBaseline from '@mui/material/CssBaseline';
-import { HashRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { renderRoutes } from 'react-router-config';
-import routes from './router';
+import router from './router';
+import Button from '@mui/material/Button';
 
 const mdTheme = createTheme();
 
 function App() {
   return (
-    <HashRouter>
-      <ThemeProvider theme={mdTheme}>
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <AppBar position="absolute">
-            <Toolbar>
-              <Typography
-                component="h1"
-                variant="h6"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-                onClick={() => {
-                  location.href = '/#/';
-                }}
-              >
-                WOJ
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Box
-            component="main"
-            sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === 'light'
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: '100vh',
-              overflow: 'auto',
-            }}
-          >
-            <Toolbar />
-            <Suspense fallback={<CircularProgress disableShrink />}>
-              {renderRoutes(routes)}
-            </Suspense>
-          </Box>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute">
+          <Toolbar>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+              onClick={() => {
+                location.href = '/#/';
+              }}
+            >
+              WOJ
+            </Typography>
+            <Button href="/#/login" variant="contained">
+              Login
+            </Button>
+            <Button href="/#/register" variant="contained">
+              Register
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Suspense fallback={<CircularProgress disableShrink />}>
+            <RouterProvider router={router} />
+          </Suspense>
         </Box>
-      </ThemeProvider>
-    </HashRouter>
+      </Box>
+    </ThemeProvider>
   );
 }
 export default App;
---
+++ src/router/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createHashRouter } from 'react-router-dom';
+import loadable from '@loadable/component';
+const Index = loadable(() => import('../pages/index'));
+const Login = loadable(() => import('../pages/login'));
+const Register = loadable(() => import('../pages/register'));
+const Page404 = loadable(() => import('../pages/page404'));
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Index />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/Register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <Page404 />,
+  },
+]);
+
+export default router;
